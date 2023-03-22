da1c774677e7d7d1a4e68ac2f5c8f56f4d836886
Plugna <info@plugna.com>
2023-03-22 15:56:09 +0100

    updates

--- dynamic-css.php
+++ dynamic-css.php
@@ -1,44 +1,76 @@
 <?php
 require_once('../../../wp-load.php');
 
-function generate_custom_color_scheme_css($colors = null) {
+$is_template = isset($_GET['template']) && $_GET['template'] === 'true';
 
-    $is_template = isset($_GET['template']) && $_GET['template'] === 'true';
+$colors = CACOS::get_custom_colors();
 
-    $colors = CACOS::get_custom_colors();
-
-    if(empty($colors)){
-        exit;
-    }
-
-    if($is_template){
-        $colors = array('$1', '$2', '$3', '$4', '$5', '$6', '$7');
-    }
-
-    $color_replacements = [];
-    $counter = 0;
-    $default_colors = CACOS::get_default_colors();
-
-    foreach ((array) $default_colors as $dc) {
-        $color_replacements[$dc] = $colors[$counter];
-        $counter++;
-    }
-
-    $input_css = ABSPATH . 'wp-admin/css/colors/blue/colors.min.css';
-    $css = file_get_contents($input_css);
-
-    foreach ($color_replacements as $search => $replace) {
-        $css = str_replace($search, $replace, $css);
-    }
-
-    return $css;
+if(empty($colors)){
+    exit;
 }
 
-//header('Content-Type: text/css');
-//header('Cache-Control: public, max-age=86400'); // Cache for 1 day
+if($is_template){
+    $colors = array('$1', '$2', '$3', '$4', '$5', '$6', '$7');
+}
+
+$color_replacements = [];
+$counter = 0;
+$default_colors = CACOS::get_default_colors();
+
+foreach ((array) $default_colors as $dc) {
+    $color_replacements[$dc] = $colors[$counter];
+    $counter++;
+}
+
+$input_css = ABSPATH . 'wp-admin/css/colors/blue/colors.min.css';
+$css = file_get_contents($input_css);
+
+foreach ($color_replacements as $search => $replace) {
+    $css = str_replace($search, $replace, $css);
+}
 
 header('Content-Type: text/css; charset=utf-8');
 header('Cache-Control: public, max-age=86400');
-echo generate_custom_color_scheme_css(CACOS::get_custom_colors());
+echo $css;
+
+//function generate_custom_color_scheme_css($colors = null) {
+//
+//    $is_template = isset($_GET['template']) && $_GET['template'] === 'true';
+//
+//    $colors = CACOS::get_custom_colors();
+//
+//    if(empty($colors)){
+//        exit;
+//    }
+//
+//    if($is_template){
+//        $colors = array('$1', '$2', '$3', '$4', '$5', '$6', '$7');
+//    }
+//
+//    $color_replacements = [];
+//    $counter = 0;
+//    $default_colors = CACOS::get_default_colors();
+//
+//    foreach ((array) $default_colors as $dc) {
+//        $color_replacements[$dc] = $colors[$counter];
+//        $counter++;
+//    }
+//
+//    $input_css = ABSPATH . 'wp-admin/css/colors/blue/colors.min.css';
+//    $css = file_get_contents($input_css);
+//
+//    foreach ($color_replacements as $search => $replace) {
+//        $css = str_replace($search, $replace, $css);
+//    }
+//
+//    return $css;
+//}
+//
+////header('Content-Type: text/css');
+////header('Cache-Control: public, max-age=86400'); // Cache for 1 day
+//
+//header('Content-Type: text/css; charset=utf-8');
+//header('Cache-Control: public, max-age=86400');
+//echo cacos_generate_custom_scheme(CACOS::get_custom_colors());
 
 
--- js/cacos.js
+++ js/cacos.js
@@ -1,14 +1,11 @@
 jQuery(document).ready(function ($) {
     var templateCss = '';
-    ///var $cacosTable = $('#custom_admin_color_scheme_row').closest('.form-table');
 
     function applyColorSchemePreview() {
         var colors = [];
-        $('.custom-admin-color-scheme-picker').each(function() {
+        $('.cacos-picker').each(function() {
             colors.push($(this).wpColorPicker('color'));
         });
-
-        console.log('colors', colors);
 
         if (templateCss) {
             var css = templateCss;
@@ -23,16 +20,17 @@ jQuery(document).ready(function ($) {
     }
 
     function fetchTemplateCss() {
-        $.get(custom_admin_color_scheme_data.plugin_url + '/dynamic-css.php?&template=true&v=1', function(data) {
+        $.get(cacos_data.plugin_url + '/dynamic-css.php?&template=true&v=1', function(data) {
             templateCss = data;
         });
     }
 
-    $('.custom-admin-color-scheme-picker').wpColorPicker({
+    $('.cacos-picker').wpColorPicker({
         change: applyColorSchemePreview,
     });
 
     fetchTemplateCss();
+
     if($('#enable_cacos:checked').length){
         applyColorSchemePreview();
     }
